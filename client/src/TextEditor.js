8849458c0c39ccde152b4a5808cbdcdddec6f5d2
onanalee <69330162+onanalee@users.noreply.github.com>
2021-07-25 10:42:16 +0900

    adf

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("http://52.78.115.28")
+    const s = io("http://54.180.140.209:3001")
     setSocket(s)
 
     return () => {
